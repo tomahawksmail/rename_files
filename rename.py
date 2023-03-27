0994f6e7808ff70458d99c017a4e3f13d56bdab8
Maksym Tsybulskyi <66479202+tomahawksmail@users.noreply.github.com>
2023-03-27 14:34:27 -0700

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,40 +1,24 @@
-#
-# This Script renamed files
-# Made by Maksym Tsybulskyi
-# 3.2023
-#
-# import shutil
+####################################################
+#   This Script renamed files in path "directory"  #
+#            Made by Maksym Tsybulskyi             #
+#                    3.2023                        #
+####################################################
 import os
 import sys
-# directory = r'C:\Users\smoke\Pictures\magma\new\Klarity Kratom\Capsules'
 directory = r'C:\Users\smoke\Pictures\magma\new'
 
 for subdir, dirs, files in os.walk(directory):
-    subdirectoryPath = os.path.relpath(subdir, directory)
-    if not os.path.isdir(subdirectoryPath):
-        print('')
-        print('-------------------------------------------')
-        i = 1
-        for filename in files:
-            if filename.find('.JPG') > 0:
-                newfilename = subdirectoryPath.split("\\")[-1] +'_'+ str(i) + '.JPG'
-                # newfilename = filename + " > " + newfilename
-                # print("newfilename = " + newfilename) # print new file name converting
-                print(os.path.abspath(newfilename))
-
-
-
-
-
-
-
-    #
-                # old_path_string = os.path.join(directory, filename)
-                # new_path_string = os.path.join(directory, newfilename)
-                # print(old_path_string) # print full old path
-                # print(new_path_string)  # print full new path
-    #             try:
-    #                 os.rename(old_path_string, new_path_string)  # rename your file
-    #             except Exception as E:
-    #                 print(E)
-            i+=1
+    print('')
+    print('-------------------------------------------')
+    i = 1
+    for filename in files:
+        if filename.find('.JPG') > 0:
+            newfilename = subdir.split("\\")[-1] + '_' + str(i) + '.JPG'
+            print(os.path.join(subdir, filename))
+            print(os.path.join(subdir, newfilename))
+            print(filename + " > " +newfilename)
+            try:
+                os.rename(os.path.join(subdir, filename), os.path.join(subdir, newfilename))  # rename your file
+            except Exception as E:
+                print(E)
+            i += 1
